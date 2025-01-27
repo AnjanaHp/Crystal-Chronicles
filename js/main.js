1b032198cd4b834d3e9c5db07067ef2e6857c166
Anjana Hariprasad <anjanahariprasad21@gmail.com>
2025-01-27 11:01:01 +0100

    implement gems

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 class Player {
     constructor() {
         this.positionX = 100;
-        this.positionY = 50;
+        this.positionY = 10;
         this.width = 50;
         this.height = 50;
 
@@ -17,20 +17,69 @@ class Player {
         this.collectorElm.style.bottom = this.positionY + "px";
     }
     moveRight() {
-        this.positionX++;
+        this.positionX += 20;
         this.updateUI();
 
     }
     moveLeft() {
-        this.positionX--;
+        this.positionX -= 20;
         this.updateUI();
+        console.log("position changed to left")
     }
 }
 
+// create class for crystals
+class Crystal {
+    constructor() {
+        this.positionX = 500;
+        this.positionY = 500;
+        this.width = 20;
+        this.height = 20;
+
+        this.createDom();
+    }
+
+    createDom() {
+
+        //Create crystal Element
+        this.crystalElm = document.createElement("div");
+
+        // add content
+        this.crystalElm.className = "crystal";
+        this.crystalElm.style.left = this.positionX + "px";
+        this.crystalElm.style.bottom = this.positionY + "px";
+        this.crystalElm.style.width = this.width + "px";
+        this.crystalElm.style.height = this.height + "px";
+
+        //append to dom
+        const parentElm = document.getElementById("playarea");
+        parentElm.appendChild(this.crystalElm);
+    }
+
+    moveDown() {
+        this.positionY--;
+        console.log("position changed");
+        this.crystalElm.style.bottom = this.positionY + "px";
+    }
+}
+
+
+
+
 const collector = new Player();
+const crystalsArr = [];
 
+setInterval(() => {
+    const crystalIs = new Crystal();
+    crystalsArr.push(crystalIs);
+    console.log("moving down")
+}, 5000);
 
-
+setInterval(() => {
+    crystalsArr.forEach((gemObj,index,arr) => {
+        gemObj.moveDown();
+    });
+},50)
 
 
 document.addEventListener("keydown", (event) => {
